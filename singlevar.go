56b9fa058ddcb888f7c95cb383a3e312486184d6
sarath6080 <sarathkmr05@gmail.com>
2018-09-28 14:15:17 +0000

    variable in go

---
+++ singlevar.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	var a int //variable declaration
+	fmt.Println("the age is :",a)
+	a = 29
+	fmt.Println("the age is :",a) 
+	a=56
+	fmt.Println("the age is :",a) 
+}
