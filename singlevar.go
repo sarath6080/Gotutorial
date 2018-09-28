46b8cfddffb7c96ee94502aeaf72eae3af19c58a
sarath6080 <sarathkmr05@gmail.com>
2018-09-28 14:28:57 +0000

    all variable syntax

--- singlevar.go
+++ singlevar.go
@@ -9,4 +9,24 @@ func main(){
 	fmt.Println("the age is :",a) 
 	a=56
 	fmt.Println("the age is :",a) 
+
+	// type inference
+	var age = 29
+	fmt.Println("the age is :",age)
+
+	// Multiple var declartion
+	var weight,height = 100,50
+	fmt.Println("the weight is", weight ,"the height is", height)
+
+	var (
+	age1 = 90
+	name = "sarath"
+	height2 int
+	)
+	fmt.Println("the name is",name , "the age is ",age1 ,"and height", height2)
+
+	//short ahnd description
+	name1,agei := "nav",40
+	fmt.Println("the name ",name1 ,"the age is",agei)
+
 }
